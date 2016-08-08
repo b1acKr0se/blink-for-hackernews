77b3b7d7b853a876c3ecb3ff1b93f20254fba4d7
Hải Nguyễn <haint.fit12@gmail.com>
2016-08-08 16:26:54 +0700

    Strikethrough comment that was deleted

--- app/src/main/java/nt/hai/blinkforhackernews/view/adapter/CommentViewHolder.java
+++ app/src/main/java/nt/hai/blinkforhackernews/view/adapter/CommentViewHolder.java
@@ -1,8 +1,12 @@
 package nt.hai.blinkforhackernews.view.adapter;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.StrikethroughSpan;
 import android.text.util.Linkify;
 import android.view.View;
 import android.widget.TextView;
@@ -13,11 +17,14 @@ import nt.hai.blinkforhackernews.utility.ColorCode;
 import nt.hai.blinkforhackernews.utility.DateUtils;
 
 public class CommentViewHolder extends RecyclerView.ViewHolder {
+    private static final StrikethroughSpan STRIKE_THROUGH_SPAN = new StrikethroughSpan();
     private TextView author, score, time, content;
     private View level;
+    private Context context;
 
     public CommentViewHolder(View view) {
         super(view);
+        context = itemView.getContext();
         author = (TextView) view.findViewById(R.id.comment_author);
         score = (TextView) view.findViewById(R.id.comment_score);
         time = (TextView) view.findViewById(R.id.comment_time);
@@ -26,11 +33,18 @@ public class CommentViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(Item item) {
-        level.setBackgroundColor(ColorCode.getInstance(itemView.getContext()).getColor(item.getLevel()));
+        level.setBackgroundColor(ColorCode.getInstance(context).getColor(item.getLevel()));
         if (!item.isLoaded()) {
             author.setText("...");
             content.setText("...");
             time.setText("");
+            score.setText("");
+            return;
+        }
+        if (item.isDeleted()) {
+            markDeleted(time, DateUtils.getReadableDate(item.getTime()));
+            content.setText("");
+            author.setText("");
             score.setText("");
             return;
         }
@@ -39,5 +53,11 @@ public class CommentViewHolder extends RecyclerView.ViewHolder {
         time.setText(DateUtils.getReadableDate(item.getTime()));
         content.setText(Html.fromHtml(item.getText() == null ? "" : item.getText()));
         Linkify.addLinks(content, Linkify.ALL);
+    }
+
+    private void markDeleted(TextView timeTextView, String text) {
+        timeTextView.setText(text, TextView.BufferType.SPANNABLE);
+        Spannable spannable = (Spannable) timeTextView.getText();
+        spannable.setSpan(STRIKE_THROUGH_SPAN, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 }
